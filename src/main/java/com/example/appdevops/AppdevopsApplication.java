e98b4b5f5bff599d866703f16d70b8ac32430c45
Piyawat Intarawichai <piyawat@orcsoft.co.th>
2025-11-04 15:56:49 +0700

    Initial Project

---
+++ src/main/java/com/example/appdevops/AppdevopsApplication.java
@@ -0,0 +1,13 @@
+package com.example.appdevops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppdevopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppdevopsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/appdevops/AppdevopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.appdevops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppdevopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
